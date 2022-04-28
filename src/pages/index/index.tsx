38596450572c55c1fbbdd4684349f52c669e90cf
齐彦鑫 <1834362615@qq.com>
2022-04-28 17:13:56 +0800

    初始化登陆页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -18,18 +18,37 @@ interface Ref<T> {
 
 export default defineComponent({
    setup:(props) => {
-    const internalInstance = getCurrentInstance()
+    const {$baseUrl} = getCurrentInstance()?.appContext.config.globalProperties as any;
     onMounted(() => {
+      uni.request({
+        url: $baseUrl+'/user/update', //仅为示例，并非真实接口地址。
+        method:'POST',
+        data: {
+            payload:{
+              nickname:'214',
+            },
+            options: {
+              email:'18@qq.com'
+            }
+        },
+        header: {
+            'content-type':'application/json',
+            'custom-header': 'hello' //自定义请求头信息
+        },
+        success: (res) => {
+            console.log(res.data);
+        }
+    });
       socket.on('connect', () => {
 				console.log(socket.auth.id, 'id') // x8WIv7-mJelg7on_ALbx
 			})
-      socket.on('user connected', (res) => {
+      socket.on('user connected', (res:any) => {
 				console.log(res, 'user connected')
 			})
-      socket.on('private message',(res) => {
+      socket.on('private message',(res:any) => {
         console.log(res,'private message')
       })
-      socket.on('connect_error', e => {
+      socket.on('connect_error',( e:any) => {
 				console.log('connect_error', e);
 			});
       console.log('mounted!',props)
